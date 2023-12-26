f3e3f16cfefea2a1f7a734a340d2ff8a6dfe26b4
maximfb <mbf.programmer@gmail.com>
2023-12-26 14:12:39 +0300

    Fixed event dto

--- src/app/events/dto/event.dto.ts
+++ src/app/events/dto/event.dto.ts
@@ -1,11 +1,16 @@
 import { Types } from 'mongoose';
-import { IsArray, IsDate, IsMongoId, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsDateString, IsMongoId, IsOptional, IsString } from 'class-validator';
 
 export class EventDto {
   @IsMongoId() eventId: Types.ObjectId;
   @IsMongoId() author: Types.ObjectId;
   @IsString() hText: string;
-  @IsOptional() @IsDate() startDate?: Date;
+  @IsOptional()
+  @IsDateString()
+  startDate?: Date;
+  @IsOptional()
+  @IsDateString()
+  stopDate?: Date;
   @IsOptional() @IsString() hImg?: string;
   @IsOptional() @IsString() addr?: string;
   @IsOptional() @IsString() category?: string;
@@ -15,5 +20,4 @@ export class EventDto {
   @IsOptional() @IsArray() favor?: Array<string>;
   @IsOptional() @IsArray() iGo?: Array<string>;
   @IsOptional() @IsArray() notGo?: Array<string>;
-  @IsOptional() @IsDate() stopDate?: Date;
 }

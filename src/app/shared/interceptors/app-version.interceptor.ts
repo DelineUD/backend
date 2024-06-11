f564fd7bcc5f270d9a6be9cc25b43a04927003b3
sk8beemo <skateor2007@gmail.com>
2024-06-12 05:14:18 +0700

    fix message

--- src/app/shared/interceptors/app-version.interceptor.ts
+++ src/app/shared/interceptors/app-version.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus } from '@nestjs/common';
+import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 /**
@@ -10,8 +10,7 @@ export class AppVersionInterceptor implements NestInterceptor {
     const req = context.switchToHttp().getRequest();
     const appVersion = req.headers['app-version'];
 
-    if (!appVersion) throw new HttpException('App-version header not found!', HttpStatus.BAD_REQUEST);
-    if (appVersion !== '1.0.8') {
+    if (!appVersion || appVersion !== '1.0.8') {
       throw new HttpException(
         'Пожалуйста, обновите ваше приложение до последней версии!',
         HttpStatus.INTERNAL_SERVER_ERROR,

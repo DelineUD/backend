3d4ef75a4bfc47bf7a8e359003413da03766311c
maximfb <mbf.programmer@gmail.com>
2024-09-12 21:23:53 +0300

    Added implements for cities

--- src/app/filters/entities/cities.entity.ts
+++ src/app/filters/entities/cities.entity.ts
@@ -1,11 +1,13 @@
 import { Document } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+import { IFilter } from '@app/filters/interfaces/filters.interface';
+
 @Schema({
   collection: 'cities',
   timestamps: true,
 })
-export class CitiesEntity extends Document {
+export class CitiesEntity extends Document implements IFilter {
   @Prop({ required: true }) name: string;
 }
 
